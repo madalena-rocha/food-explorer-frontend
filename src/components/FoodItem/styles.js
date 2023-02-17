277ff96a1de8b115420315c03fa2f5e0fbe5ca71
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-02-17 15:09:40 -0300

    :sparkles: add component FoodItem

---
+++ src/components/FoodItem/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme, isNew }) =>
+    isNew ? "transparent" : theme.COLORS.GRAY_400};
+  border: ${({ theme, isNew }) =>
+    isNew ? `1px dashed ${theme.COLORS.GRAY_300}` : "none"};
+  
+  margin-bottom: 8px;
+  margin-right: 16px;
+  border-radius: 8px;
+  padding-right: 16px;
+  
+  > button {
+    border: none;
+    background: none;
+
+    display: flex;
+    align-items: center;
+    
+    color: ${({ theme, isNew }) =>
+    isNew ? theme.COLORS.GRAY_300 : theme.COLORS.WHITE};
+  }
+  
+  > input {
+    height: 32px;
+    width: 100%;
+    
+    padding: 8px 8px 8px 16px;
+    border: none;
+    
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+  }
+`;
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,12 +2,18 @@ import { Container } from "./styles";
 
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
+import { FoodItem } from '../../components/FoodItem';
 import { Textarea } from '../../components/Textarea';
 
 export function New() {
   return (
     <Container>
       <Header isAdmin />
+
+      <Section title="Ingredientes">
+        <FoodItem value="Pão Naan" />
+        <FoodItem isNew placeholder="Adicionar" />
+      </Section>
 
       <Section title="Descrição">
         <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
