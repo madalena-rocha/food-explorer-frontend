8a159b0b7ba2f6f9aaedd6dbff4ed6cbc643d036
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-02-18 11:11:43 -0300

    :zap: create page Home

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
 
-import { Dish } from "./pages/Dish";
+import { Home } from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Dish />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+
+export function Home() {
+  return (
+    <Container>
+      <Header isAdmin />
+
+      <Footer />
+    </Container>
+  );
+}
