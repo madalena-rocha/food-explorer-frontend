443020aab99e84ff52c36871e333f596d29d0cef
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-02-21 22:42:24 -0300

    :zap: finish desktop SignIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,43 @@
-import { Container } from "./styles";
+import { Link } from "react-router-dom";
 
+import { Container, Form, Brand } from "./styles";
+
+import { Section } from '../../components/Section';
 import { Input } from '../../components/Input';
 import { Button } from "../../components/Button";
+
+import brand from "../../assets/brand.svg";
 
 export function SignIn() {
   return (
     <Container>
-      <Input placeholder="Exemplo: exemplo@exemplo.com.br" />
-      <Input placeholder="No mínimo 6 caracteres" />
-      
-      <Button title="Entrar" />
+      <Brand>
+        <img src={brand} alt="Logo" />
+      </Brand>
+
+      <Form>
+        <h2>Faça seu login</h2>
+
+        <Section title="Email">
+          <Input 
+            placeholder="Exemplo: exemplo@exemplo.com.br" 
+            type="text"
+          />
+        </Section>
+
+        <Section title="Senha">
+          <Input 
+            placeholder="No mínimo 6 caracteres" 
+            type="password"
+          />
+        </Section>
+
+        <Button title="Entrar" />
+
+        <Link to="/register">
+          Criar uma conta
+        </Link>
+      </Form>
     </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,3 +1,57 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  margin-inline: 154px 108px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Brand = styled.div`
+  display: flex;
+  
+  > svg {
+    width: 324px;
+    height: 48px;
+  }
+`;
+
+export const Form = styled.form`
+  min-width: 476px;
+  padding: 64px;
+  border-radius: 16px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  > h2 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 140%;
+    text-align: center;
+  }
+
+  > section h2 {
+    margin-bottom: 8px;
+  }
+
+  > section input {
+    border: 1px solid ${({ theme }) => theme.COLORS.WHITE};
+    border-radius: 5px;
+  }
+  
+  > a {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    text-align: center;
+
+    font-family: "Poppins", sans-serif;
+    font-size: 14px;
+    line-height: 24px;
+  }
+`;
