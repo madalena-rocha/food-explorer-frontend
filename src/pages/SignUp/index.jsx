2cce414448c13a8a5c868bb4d1a6450af7df8762
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-02-21 22:44:11 -0300

    :zap: finish desktop SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,50 @@
-import { Container } from "./styles";
+import { Link } from "react-router-dom";
+
+import { Container, Form, Brand } from "./styles";
+
+import { Section } from '../../components/Section';
+import { Input } from '../../components/Input';
+import { Button } from "../../components/Button";
+
+import brand from "../../assets/brand.svg";
 
 export function SignUp() {
   return (
     <Container>
+      <Brand>
+        <img src={brand} alt="Logo" />
+      </Brand>
+
+      <Form>
+        <h2>Crie sua conta</h2>
+
+        <Section title="Seu nome">
+          <Input 
+            placeholder="Exemplo: Maria da Silva" 
+            type="text"
+          />
+        </Section>
+
+        <Section title="Email">
+          <Input 
+            placeholder="Exemplo: exemplo@exemplo.com.br" 
+            type="text"
+          />
+        </Section>
+
+        <Section title="Senha">
+          <Input 
+            placeholder="No mínimo 6 caracteres" 
+            type="password"
+          />
+        </Section>
+
+        <Button title="Criar conta" />
+
+        <Link to="/">
+          Já tenho uma conta
+        </Link>
+      </Form>
     </Container>
   );
 }
