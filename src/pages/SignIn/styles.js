711c36e26663c108e517002eb54173c87e2418fa
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-04-17 09:11:56 -0300

    :zap: finish mobile SignIn page

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -2,39 +2,52 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
-  margin-inline: 154px 108px;
+  margin-inline: 65px 47px;
 
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 73px;
+
+  @media (min-width: 1024px) {
+    margin-inline: 154px 108px;
+
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 0;
+  }
 `;
 
 export const Brand = styled.div`
   display: flex;
+  margin-inline: auto;
   
   > svg {
-    width: 324px;
-    height: 48px;
+    width: 278.05px;
+    height: 44px;
+  }
+
+  @media (min-width: 1024px) {
+    margin-inline: 0;
+
+    > svg {
+      width: 324px;
+      height: 48px;
+    }
   }
 `;
 
 export const Form = styled.form`
-  min-width: 476px;
-  padding: 64px;
-  border-radius: 16px;
+  width: 100%;
+  max-width: 316px;
 
   display: flex;
   flex-direction: column;
   gap: 32px;
 
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
-
   > h2 {
-    font-family: "Poppins", sans-serif;
-    font-weight: 500;
-    font-size: 32px;
-    line-height: 140%;
-    text-align: center;
+    display: none;
   }
 
   > section h2 {
@@ -42,8 +55,8 @@ export const Form = styled.form`
   }
 
   > section input {
-    border: 1px solid ${({ theme }) => theme.COLORS.WHITE};
-    border-radius: 5px;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_500};
+    border-radius: 8px;
   }
   
   > a {
@@ -53,5 +66,30 @@ export const Form = styled.form`
     font-family: "Poppins", sans-serif;
     font-size: 14px;
     line-height: 24px;
+  }
+  
+  @media (min-width: 1024px) {
+    min-width: 476px;
+    padding: 64px;
+    border-radius: 16px;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+    > h2 {
+      display: initial;
+
+      font-family: "Poppins", sans-serif;
+      font-weight: 500;
+      font-size: 32px;
+      line-height: 140%;
+      text-align: center;
+    }
+
+    > section input {
+      background-color: transparent;
+
+      border: 1px solid ${({ theme }) => theme.COLORS.WHITE};
+      border-radius: 5px;
+    }
   }
 `;
