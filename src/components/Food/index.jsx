e0ab429a77d180df4329785794c7587fe9dade7d
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-06-05 22:12:06 -0300

    :sparkles: add cart

--- src/components/Food/index.jsx
+++ src/components/Food/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { BiPencil } from "react-icons/bi";
 import { FiHeart } from "react-icons/fi";
 import { RxCaretRight } from "react-icons/rx";
@@ -5,6 +7,7 @@ import { RxCaretRight } from "react-icons/rx";
 import { useMediaQuery } from "react-responsive";
 import theme from "../../styles/theme";
 
+import { useParams, useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
 
 import { Container, Title, Order } from "./styles";
@@ -13,6 +16,11 @@ import { Button } from "../../components/Button";
 
 export function Food({ data, isAdmin, isFavorite, updateFavorite, handleDetails, ...rest }) {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const [number, setNumber] = useState(1);
 
   const handleFavorite = async () => {
     try {
@@ -26,10 +34,33 @@ export function Food({ data, isAdmin, isFavorite, updateFavorite, handleDetails,
     }
   };
 
+  function handleEdit() {
+    navigate(`/edit/${params.id}`);
+  }
+
+  async function handleInclude() {
+    try {
+      const cartItem = {
+        dish_id: data.id,
+        name: data.name,
+        quantity: number,
+      };
+
+      await api.post('/carts', { cart_items: [cartItem] })
+      alert("Prato adicionado ao carrinho!");
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível adicionar ao carrinho.");
+      }
+    }
+  }
+
   return (
     <Container {...rest} isAdmin={isAdmin}>
       {isAdmin ? (
-        <BiPencil size={"2.4rem"} />
+        <BiPencil size={"2.4rem"} onClick={handleEdit} />
       ) : (
         <FiHeart
           size={"2.4rem"}
@@ -58,7 +89,7 @@ export function Food({ data, isAdmin, isFavorite, updateFavorite, handleDetails,
       {!isAdmin && 
         <Order>
           <NumberPicker />
-          <Button title="incluir" />
+          <Button title="incluir" onClick={handleInclude} />
         </Order>
       }
     </Container>
