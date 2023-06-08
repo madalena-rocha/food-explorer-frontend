600b7d923a85a5627024b04d71854dd60b29e4a8
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-06-08 15:47:42 -0300

    :zap: handle new and signout

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,24 +1,39 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
+
 import { Container } from "./styles";
 
 import { Header } from '../../components/Header';
 import { Search } from "../Search";
 import { ButtonText } from "../ButtonText";
 
-export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen, setSearch }) {
+export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen, setSearch, isDisabled }) {
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+
+  function handleNew() {
+    navigate("/new");
+  }
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
   return (
     <Container isMenuOpen={isMenuOpen}>
       <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
       <main>
-        <Search setSearch={setSearch} />
+        <Search isDisabled={isDisabled} setSearch={setSearch} />
 
-        {isAdmin && 
-          <ButtonText>
+        {isAdmin ? (
+          <ButtonText onClick={handleNew}>
             Novo prato
           </ButtonText>
-        }
+        ) : null}
 
-        <ButtonText>
+        <ButtonText onClick={handleSignOut}>
           Sair
         </ButtonText>
       </main>
